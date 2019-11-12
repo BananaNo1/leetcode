6c9a67e012d126674147789d3c1874637a464e02
lei <893052525@qq.com>
2019-11-12 18:47:35 +0800

    19-11-12

--- src/simple/AddBinary.java
+++ src/simple/AddBinary.java
@@ -2,10 +2,38 @@ package simple;
 
 /**
  * @ClassName AddBinary
- * @Description TODO
+ * @Description 给定两个二进制字符串，返回他们的和（用二进制表示）。
+ * <p>
+ * 输入为非空字符串且只包含数字 1 和 0。
  * @Author leis
  * @Date 2019/11/12 17:38
  * @Version 1.0
  **/
 public class AddBinary {
+
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int len1 = a.length() - 1;
+        int len2 = b.length() - 1;
+        int is = 0;
+        while (len1 >= 0 || len2 >= 0) {
+            int sum = is;
+            sum += (len1 >= 0 ? a.charAt(len1) - '0' : 0);
+            sum += (len2 >= 0 ? b.charAt(len2) - '0' : 0);
+            sb.append(sum % 2);
+            is = sum / 2;
+            len1--;
+            len2--;
+        }
+        if (is == 1) {
+            sb.append(1);
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String a = "1011";
+        String b = "1010";
+        System.out.println(addBinary(a, b));
+    }
 }
--- src/simple/MySqrt.java
+++ src/simple/MySqrt.java
@@ -2,10 +2,49 @@ package simple;
 
 /**
  * @ClassName MySqrt
- * @Description TODO
+ * @Description 实现 int sqrt(int x) 函数。
+ * 计算并返回 x 的平方根，其中 x 是非负整数
  * @Author leis
  * @Date 2019/11/12 18:04
  * @Version 1.0
  **/
 public class MySqrt {
+
+    public static int mySqrt(int x) {
+        int min = 1;
+        int max = x;
+        while (min < max) {
+            int mid = min + (max - min) / 2 + 1;
+            if (mid <= x / mid) {
+                min = mid;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return max;
+    }
+
+    public static int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = (left + right + 1) >>> 1;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(9));
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(search(nums, 6));
+    }
 }
