b8c4d1805da5945ec4592ab31f3cf0895253e671
lei <893052525@qq.com>
2019-11-12 15:45:14 +0800

    '19-11-12'

---
+++ src/simple/LengthOfLastWord.java
@@ -0,0 +1,11 @@
+package simple;
+
+/**
+ * @ClassName LengthOfLastWord
+ * @Description TODO
+ * @Author leis
+ * @Date 2019/11/11 17:18
+ * @Version 1.0
+ **/
+public class LengthOfLastWord {
+}
---
+++ src/simple/PlusOne.java
@@ -0,0 +1,11 @@
+package simple;
+
+/**
+ * @ClassName PlusOne
+ * @Description TODO
+ * @Author leis
+ * @Date 2019/11/11 17:52
+ * @Version 1.0
+ **/
+public class PlusOne {
+}
