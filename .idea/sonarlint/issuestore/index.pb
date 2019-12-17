a8a5a3d44e739fd3c993b723d99a67225b1aea2a
lei <893052525@qq.com>
2019-12-17 18:37:31 +0800

    '2019-12-17'

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -27,3 +27,46 @@ L
 src/ThinkJava/SweetShop.java,8\0\807081a146b09fdddba7ba2c7713602dc87a07cf
 L
 src/ThinkJava/FinalTest.java,f\4\f4510d8a0e18e0ab2adaa76a7011a12d3535fe73
+K
+src/simple/MaxSubArray.java,3\b\3b8b340d945593d9bc5208096fc46fb4470456db
+P
+ src/simple/LengthOfLastWord.java,7\2\72897e71ce049f2d50f4d5180178ee3b419df028
+G
+src/simple/PlusOne.java,8\9\89b99c7a7ac1a770cc0b25a371c7a7e99a4ebe8f
+I
+src/simple/AddBinary.java,b\9\b96b786d89889c6803782b69c100af33b526e304
+F
+src/simple/MySqrt.java,5\f\5f3ee583073b5ccf4a47ab42b17088e223b48174
+K
+src/simple/ClimbStairs.java,8\6\8699cb6e5929569317f0e459fb1c1a083010a650
+E
+src/simple/Merge.java,e\c\ec99b92d091753680ccf6903823f508ee50413fa
+P
+ src/simple/DeleteDuplicates.java,a\8\a8fbb2e5b163b10731b30ab2d9526497c55772a0
+J
+src/simple/IsSameTree.java,e\3\e38205c7bb64c99aa5ce23f923e9e2fc9ad68996
+K
+src/simple/IsSymmetric.java,7\a\7a84af65d27747f863f7a3ebd877f4790d37b557
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+H
+src/simple/MaxDepth.java,d\f\df4ba4e6b1f7556e0c62695a7fdf6f6e6908803c
+P
+ src/simple/LevelOrderBottom.java,d\9\d953efd4e9c2dfae40b81b51a04bf6afa3b02961
+Y
+)src/difficult/FindMedianSortedArrays.java,2\0\20903d89bb2991c8b00b6fbade50c2c2615d15da
+Q
+!src/medium/LongestPalindrome.java,3\d\3d5d87ffd5e1de292ca20b3795281f17194da37d
+G
+src/medium/Convert.java,0\a\0ae11735c00011a14653ca4421cca3c5bad682ae
+F
+src/medium/MyAtoi.java,1\7\17cd177eae42fff41f32bd5ef6112589b3950bef
+J
+src/difficult/IsMatch.java,f\d\fdaf87391a501d2eea9a3c068d074f1d4a7d22d8
+G
+src/medium/MaxArea.java,6\c\6c0b2d30b5be80a594c0002e8a542b9b0fd3f55c
+J
+src/medium/IntToRoman.java,9\3\930fc76a5a3beda8a738708b054e044865d57f6b
+H
+src/medium/ThreeSum.java,9\e\9e5766ecbe2fad671e94fe7aabec597c1f757580
--- src/medium/ThreeSum.java
+++ src/medium/ThreeSum.java
@@ -1,23 +1,63 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * @ClassName ThreeSum
  * @Description 三数之和
  * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
- *注意：答案中不可以包含重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
  * @Author leis
  * @Date 2019/11/29 17:17
  * @Version 1.0
  **/
 public class ThreeSum {
-    public List<List<Integer>> threeSum(int[] nums) {
-
-        return null;
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int length = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < length; i++) {
+            if (nums[i] > 0) {
+                continue;
+            }
+            int left = i + 1;
+            int right = length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    result.add(list);
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> lists = threeSum(nums);
+        for (int i = 0; i < lists.size(); i++) {
+            System.out.println(lists.get(i));
+        }
     }
 }
