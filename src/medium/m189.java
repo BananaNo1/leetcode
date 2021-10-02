1d80ee6e4ff21095c223f36541cb028fd2f215a3
unknown <893052525@qq.com>
2021-10-02 22:11:10 +0800

    2021-10-02

--- src/medium/m189.java
+++ src/medium/m189.java
@@ -1,2 +1,36 @@
-package medium;public class m189 {
+package medium;
+
+/**
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ */
+public class m189 {
+
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int[] arr = new int[k];
+        int j = 0;
+        for (int i = nums.length-k; i < nums.length; i++) {
+            arr[j] = nums[i];
+            j++;
+        }
+        for (int i = nums.length - 1; i >= k; i--) {
+            nums[i] = nums[i - k];
+
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = arr[i];
+        }
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(nums, k);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
--- src/simple/S977.java
+++ src/simple/S977.java
@@ -1,2 +1,35 @@
-package simple;public class S977 {
+package simple;
+
+import java.util.Arrays;
+
+public class S977 {
+
+    public static int[] sortedSquares1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        int s = 0;
+        int e = nums.length - 1;
+
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+        sortedSquares1(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
