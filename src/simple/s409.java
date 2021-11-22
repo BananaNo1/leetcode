5bf042789c0baeda02d8ffddb5c1c74c141b06d7
Administrator <893052525@qq.com>
2021-11-22 20:50:25 +0800

    2021-11-22

---
+++ src/simple/s409.java
@@ -0,0 +1,26 @@
+package simple;
+
+public class s409 {
+
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            count[c]++;
+        }
+        int ans = 0;
+        for (int v : count) {
+            ans += v / 2 * 2;
+            if (v % 2 == 1 && ans % 2 == 0) {
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        char c = 'A';
+        System.out.println();
+    }
+}
