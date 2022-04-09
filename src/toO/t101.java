b2b2ea7cb9541e5a00dd93ccb1948a39f5ea6cbe
Administrator <893052525@qq.com>
2022-04-09 11:32:30 +0800

    22-4-9

---
+++ src/toO/t101.java
@@ -0,0 +1,39 @@
+package toO;
+
+public class t11 {
+    /**
+     * 跳台阶
+     *
+     * @param n
+     * @return
+     */
+    public static int numWays(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1 || n == 2)
+            return n;
+        return numWays(n - 1) + numWays(n - 2);
+    }
+
+    public static int numWays1(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int dp1 = 1, dp2 = 2;
+        int res = 0;
+        for (int i = 3; i <= n; i++) {
+            res = (dp1 + dp2) % 1000000007;
+            dp1 = dp2;
+            dp2 = res;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numWays1(7));
+    }
+}
