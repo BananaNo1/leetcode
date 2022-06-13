7913b54211c27d4e2937c2f5b550e6f3ca052137
Administrator <893052525@qq.com>
2022-06-13 16:03:52 +0800

    22-06-13

---
+++ src/toO/t55_2.java
@@ -0,0 +1,54 @@
+package toO;
+
+public class t55_2 {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // method
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+    // method
+    public boolean isBalanced1(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    private int recur(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = recur(root.left);
+        if (left == -1) {
+            return -1;
+        }
+        int right = recur(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
